1d93870e39e3a030b02938b9e09c904e56ba2cb4
Neil Aldea <njaldea@gmail.com>
2025-05-17 17:27:54 +0800

    [service] v1.0.0

--- src/src/codec.cpp
+++ src/src/codec.cpp
@@ -1,34 +1,29 @@
 #include <nil/service/codec.hpp>
-#include <nil/xalt/type_id.hpp>
 
 #include "utils.hpp"
 
 namespace nil::service::detail
 {
-    std::size_t serialize(xalt::type_id<std::string> tag, void* output, const std::string& message)
+    std::size_t serialize(xalt::tlist<std::string> tag, void* output, const std::string& message)
     {
         std::copy(message.begin(), message.end(), static_cast<std::uint8_t*>(output));
         return size(tag, message);
     }
 
-    std::string deserialize(
-        xalt::type_id<std::string> /* t */,
-        const void* input,
-        std::uint64_t size
-    )
+    std::string deserialize(xalt::tlist<std::string> /* t */, const void* input, std::uint64_t size)
     {
         return std::string(static_cast<const char*>(input), size); // NOLINT
     }
 
 // NOLINTNEXTLINE(cppcoreguidelines-macro-usage)
 #define NIL_SERVICE_CODEC_DEFINE(TYPE)                                                             \
-    std::size_t serialize(xalt::type_id<TYPE> tag, void* output, TYPE data)                        \
+    std::size_t serialize(xalt::tlist<TYPE> tag, void* output, TYPE data)                          \
     {                                                                                              \
         const auto typed = utils::to_array(data);                                                  \
         std::copy(typed.begin(), typed.end(), static_cast<std::uint8_t*>(output));                 \
         return size(tag, data);                                                                    \
     }                                                                                              \
-    TYPE deserialize(xalt::type_id<TYPE>, const void* input, std::uint64_t size)                   \
+    TYPE deserialize(xalt::tlist<TYPE>, const void* input, std::uint64_t size)                     \
     {                                                                                              \
         (void)size;                                                                                \
         return utils::from_array<TYPE>(static_cast<const std::uint8_t*>(input));                   \
